299ead83aef33943c402a8f5774934d90bbde005
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2020-10-13 10:28:00 +0200

    Add files via upload

---
+++ Cotxo1.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agents;
+
+/**
+ *
+ * @author Ramon
+ */
+
+// Exemple de Cotxo molt bàsic
+
+
+public class Cotxo1 extends Agent {
+
+    static final boolean DEBUG = false;
+
+    static final int ESQUERRA = 0;
+    static final int CENTRAL = 1;
+    static final int DRETA = 2;
+    static final int COTXE = 1;
+    
+    //int VELOCITATTOPE = 5;
+    int VELOCITATFRE = 3;
+
+    Estat estat;
+    int espera = 0;
+
+    double desquerra, ddreta, dcentral;
+
+
+    public Cotxo1(Agents pare) {
+        super(pare, "Rapido", "imatges/Coche_Koenigsegg_One.png");
+    }
+
+    @Override
+    public void inicia() {
+        setAngleVisors(40);
+        setDistanciaVisors(350);
+        setVelocitatAngular(9);
+    }
+
+    @Override
+    public void avaluaComportament() {
+
+        estat = estatCombat();  // Recuperam la informació actualitzada de l'entorn
+
+        // Si volem repetir una determinada acció durant varies interaccions
+        // ho hem de gestionar amb una variable (per exemple "espera") que faci
+        // l'acció que volem durant el temps que necessitem
+        
+        if (espera > 0) {  // no facis res, continua amb el que estaves fent
+            espera--;
+            return;
+        } else {
+                  
+            if (estat.enCollisio && estat.distanciaVisors[CENTRAL] < 15) // evita fer-ho marxa enrera
+            {
+                noGiris();
+
+                if (estat.distanciaVisors[CENTRAL] > 20) {
+                    endavant(4);
+                    return;
+                }
+
+                enrere(4);
+                espera = 30;
+                return;
+            }
+
+            ddreta = estat.distanciaVisors[DRETA];
+            desquerra = estat.distanciaVisors[ESQUERRA];
+            dcentral = estat.distanciaVisors[CENTRAL];
+
+            if (dcentral > 170) {
+                endavant(marcha(estat));
+            }
+            
+            if (estat.objecteVisor[CENTRAL] == COTXE)
+            {
+                dispara();
+            }
+
+            // Per si vull anar el més recte possible: no sempre és la manera més ràpida
+            if ((desquerra > 40) && (ddreta > 40) && dcentral > 180) {
+                endavant(marcha(estat));
+                noGiris();
+                return;
+            }
+
+            if (ddreta > desquerra) {
+                dreta();
+            } else {
+                esquerra();
+            }
+            if (dcentral < 140){
+                endavant(frenar(dcentral));
+            }
+        }
+    }
+    
+    public int  frenar (double distancia)
+    {
+        if (distancia < 40)
+        {
+            return 1;
+        }
+        else if (distancia < 60)
+        {
+            return 2;
+        }
+        else if (distancia < 100)
+        {
+            return 3;
+        }
+        else
+        {
+            return 4;
+        }
+    }
+
+    public int  marcha (Estat estado)
+    {
+        if (estado.velocitat < 110)
+        {
+            return 1;
+        }
+        else if (estado.velocitat < 200)
+        {
+            return 2;
+        }
+        else if (estado.velocitat < 250)
+        {
+            return 3;
+        }
+        else if (estado.velocitat < 300)
+        {
+            return 4;
+        }
+        else
+        {
+            return 5;
+        }            
+   }
+}
+
